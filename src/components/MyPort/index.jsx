597c548aadc28f420f480474a2875bb97b2ed5b3
Rafael F. Souza <rafaelf.souzan02@gmail.com>
2023-12-19 11:58:55 -0300

    Atualizado

--- src/components/MyPort/index.jsx
+++ src/components/MyPort/index.jsx
@@ -2,51 +2,34 @@ import React from "react";
 
 import './styles.css';
 
-import img0 from '../../imagens_design/Arte-01.png';
-import img1 from '../../imagens_design/Arte-05-Matricular-Agora.png';
-import img2 from '../../imagens_design/Batman-Rafael.png';
-import img3 from '../../imagens_design/Flyer-CR7-real-madrid.png';
-import img4 from '../../imagens_design/LOGO-rose.png';
-import img5 from '../../imagens_design/Natal-01.png';
-import img6 from '../../imagens_design/doritos.png';
-import img7 from '../../imagens_design/amouage.png';
-import img8 from '../../imagens_design/aula-01---Gatorade.png';
+import img from '../Header/img/Logo-Cor-WhiteBlack.png'
 
 export const MyPort = () => {
     return (
         <div className="portfolio">
             <div className="header-port">
                 <h1>My Portfolio</h1>
-                <a href="portfolio">Ver mais</a>
+                <a href="/">Em breve</a>
             </div>
 
             <div className="projetos">
                 <div className="card">
-                    <img src={img0} alt="" />
+                    <img src={img} alt="" />
                 </div>
                 <div className="card">
-                    <img src={img1} alt="" />
+                    <img src={img} alt="" />
                 </div>
                 <div className="card">
-                    <img src={img2} alt="" />
+                    <img src={img} alt="" />
                 </div>
                 <div className="card">
-                    <img src={img3} alt="" />
+                    <img src={img} alt="" />
                 </div>
                 <div className="card">
-                    <img src={img4} alt="" />
+                    <img src={img} alt="" />
                 </div>
                 <div className="card">
-                    <img src={img5} alt="" />
-                </div>
-                <div className="card">
-                    <img src={img6} alt="" />
-                </div>
-                <div className="card">
-                    <img src={img7} alt="" />
-                </div>
-                <div className="card">
-                    <img src={img8} alt="" />
+                    <img src={img} alt="" />
                 </div>
             </div>
         </div>
--- src/templates/Home/App.js
+++ src/templates/Home/App.js
@@ -2,37 +2,22 @@ import React from 'react';
 
 import './App.css';
 
-import { Preload } from '../../components/Preload';
 import { Header } from '../../components/Header';
 import { Banners } from '../../components/Banners';
 import { Sobre } from '../../components/Sobre';
 import { MyPort } from '../../components/MyPort';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Portfolio } from '../Portfolio';
 import { Footer } from '../../components/Footer';
 
-const Home = () => {
-  return(
-    <>
-    <Preload />
-    <Header />
-    <Banners />
-    <Sobre />
-    <MyPort />
-    <Footer />
-    </>
-  );
-}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/portfolio' element={<Portfolio />}/>
-        <Route path='*' element={<Home />}/>
-      </Routes>
-    </BrowserRouter>
+    <div className='container'>
+      <Header />
+      <Banners />
+      <Sobre />
+      <MyPort />
+      <Footer />
+    </div>
   );
 }
 
